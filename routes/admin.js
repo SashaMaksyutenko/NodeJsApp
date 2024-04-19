b49ffb7fb120f52da2231e5808efa61158cae8f9
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-04-19 18:19:13 +0300

    Enhancing the App

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 const path=require('path');
 const express=require('express');
-const productsController=require('../controllers/products')
+const adminController=require('../controllers/admin')
 const { title } = require('process');
 const router=express.Router();
-router.get('/add-product',productsController.getAddProduct);
-router.post('/add-product',productsController.postAddProduct);
+router.get('/add-product',adminController.getAddProduct);
+router.get('/products',adminController.getProducts);
+router.post('/add-product',adminController.postAddProduct);
 module.exports=router;
