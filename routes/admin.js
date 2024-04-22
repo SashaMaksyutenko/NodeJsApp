ddb5af00464c5de220e824b7ba2959000cfd8aa9
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-04-22 16:06:32 +0300

    Dynamic Routes $  Advanced Models

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,7 @@ const router=express.Router();
 router.get('/add-product',adminController.getAddProduct);
 router.get('/products',adminController.getProducts);
 router.post('/add-product',adminController.postAddProduct);
+router.get('/edit-product/:productId',adminController.getEditProduct);
+router.post('/edit-product',adminController.postEditroduct);
+router.post('/delete-product',adminController.postDeleteProduct);
 module.exports=router;
