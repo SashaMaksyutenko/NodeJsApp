c598b0feb2b2a695cd4125968a8ceb4ffb05772d
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-05-21 01:52:48 +0300

    Adding payments (using Stripe) completed

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.use(authRoutes);
 app.get("/500", errorController.get500);
 app.use(errorController.get404);
 app.use((error, req, res, next) => {
+  console.log(error.message);
   res.status(500).render("500", {
     pageTitle: "Error!",
     path: "/500",
