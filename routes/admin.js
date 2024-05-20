73d45b0e56fdc6b07dd509d6491e3530d6369287
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-05-20 17:17:44 +0300

    Async requests on client side

--- routes/admin.js
+++ routes/admin.js
@@ -31,5 +31,5 @@ router.post("/edit-product", [
   ],
   isAuth, 
   adminController.postEditProduct);
-router.post("/delete-product", isAuth, adminController.postDeleteProduct);
+router.delete("/product/:productId", isAuth, adminController.deleteProduct);
 module.exports = router;
